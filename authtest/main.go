d3a0360a6af3c5ce521b8f59f81bba34d8f1fdde
mikebway <mikebway@mikebway.com>
2023-03-11 16:23:00 -0600

    Return 404 for favicon.ico requests

--- authtest/main.go
+++ authtest/main.go
@@ -14,7 +14,10 @@ var (
 // main is the command line entry point to the application.
 func main() {
 
-	// Route request for the root URL path
+	// Route the annoying requests for favicon.ico
+	http.HandleFunc("/favicon.ico", handleFavicon)
+
+	// Route request for the root URL path and everything else under it
 	http.HandleFunc("/", handleRoot)
 
 	// Start the server
@@ -23,18 +26,24 @@ func main() {
 	log.Fatal(http.ListenAndServe(serverAddress, nil))
 }
 
-// handleRoot is the HTTP request handler for the root path of this very crude website.
+// handleRoot is the primary HTTP request handler for the root path and everything under it for this very crude website.
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 	// Bump the count of requests seen
 	requestCount++
 
 	// Log the URL that we have been asked for etc
-	log.Printf("handling request = %s, request count = %d", r.URL.Path, requestCount)
+	log.Printf("handling request = %s, request count = %d\n", r.URL.Path, requestCount)
 
 	// Dump some text onto the response
 	_, _ = fmt.Fprintf(w, "Path:\t\t%q\n", r.URL.Path)
 	_, _ = fmt.Fprintf(w, "Count:\t\t%d\n", requestCount)
+}
+
+// handleFavicon is the HTTP request handler for "/favicon.ico" requests: 404 NOT FOUND it!
+func handleFavicon(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling request = %s\n", r.URL.Path)
+	http.Error(w, "Not Found", http.StatusNotFound)
 }
 
 // requestCounter tracks the number of requests, logging the count has it increases.
